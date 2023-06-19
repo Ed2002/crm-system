e24b4a057f1f2f64a8a16b4ccded27493b8f3bc7
Ed2002 <ed49516@gmail.com>
2023-06-19 20:10:09 -0300

    Axios

--- src/Views/Client.tsx
+++ src/Views/Client.tsx
@@ -1,4 +1,26 @@
+import { useEffect } from "react";
+import axios from 'axios';
+
 export const Client = () => {
+
+    useEffect(()=>{
+        const exhttp = () => {
+            axios({
+                method: "get",
+                url: "https://jsonplaceholder.typicode.com/todos/1",
+            }).then(res => {
+                console.log(res);
+            }).catch(err => {
+                alert(err);
+            });
+        }
+
+        return () => exhttp();
+    },[]);
+
+   
+
+
     return (
         <div>
             <h1>Client</h1>
