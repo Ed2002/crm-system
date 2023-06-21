e71f23758d968f654380821f9926435686ef51b9
AndréMeyer <andre.meyer@smartconsulting.com.br>
2023-06-20 22:09:25 -0300

    updates

--- src/Views/Client.tsx
+++ src/Views/Client.tsx
@@ -1,7 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { API } from "../services/axios";
+import { Page } from "../components/Page";
+import { LoadButton } from "../components/Buttons/LoadButton";
+import { Input } from "../components/Forms/Input";
+import { TBody, THead, Table, Td, Th, Tr } from "../components/Table";
+import { Form } from "@unform/web";
+import { FormHandles } from "@unform/core";
+import { SelectInput } from "../components/Forms/Select";
+import { MenuItem } from "@mui/material";
 
 export const Client = () => {
+
+    const formRef = useRef<FormHandles>(null);
+
+    function teste() {
+        API.get('https://jsonplaceholder.typicode.com/todos/1')
+        .then(({data}) => {
+            console.log(data);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    };
 
     useEffect(()=>{
         const exhttp = () => {
@@ -16,13 +36,43 @@ export const Client = () => {
         return () => exhttp();
     },[]);
 
-   
-
+function handleSubmit(data:any) {
+    console.log(data);
+}
 
     return (
-        <div>
-            <h1>Client</h1>
-            <p>{import.meta.env.VITE_TESTE}</p>
-        </div>
+
+        <Page Title="Clientes">
+            <Form ref={formRef} onSubmit={handleSubmit}>
+                <Input name="name" label="Nome"/>
+                <Input name="email" label="E-mail"/>
+                <LoadButton variant="contained" name="submit" title="Pesquisar" type="submit"/>
+                <LoadButton variant="contained" name="submit" title="Cadastrar" type="submit"/>
+            </Form>
+
+            <Table>
+                <THead>
+                    <tr>
+                        <Th>Nome</Th>
+                        <Th>E-mail</Th>
+                        <Th>Telefone</Th>
+                        <Th>Status</Th>
+                        <Th>Ações</Th>
+                    </tr>
+                </THead>
+                <TBody>
+                    <Tr>
+                        <Td>Teste1</Td>
+                        <Td>Teste1</Td>
+                        <Td>Teste1</Td>
+                        <Td>Teste1</Td>
+                        <Td>Teste1</Td>
+                    </Tr>
+                </TBody>
+            </Table>
+        </Page>
     )
-};
+
+}
+
+export default Client;
--- src/Views/Home.tsx
+++ src/Views/Home.tsx
@@ -7,27 +7,12 @@ function Home() {
 
   
   return (
-      <Page Title="Project X">
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Card style={{ flex: '1', marginRight: '10px' }}>
-            <h3>Novos Clientes Cadastrados</h3>
-          </Card>
-  
-          <Card style={{ flex: '1', marginRight: '10px' }}>
-            <h3>Clientes Aguardando Atendimento</h3>
-          </Card>
-  
-          <Card style={{ flex: '1' }}>
-            <h3>Finalizados</h3>
-          </Card>
-        </div>
 
-        <Card>
-          <h1>Relatório Aqui</h1>
-        </Card>
+    <Page Title="Forms">
+    
+    </Page>
+  )
 
-      </Page>
-  )  
 }
 
 export default Home;
