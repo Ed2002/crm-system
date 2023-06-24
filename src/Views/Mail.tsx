093682bf3ff9c8a7ea7297dc833c206267b7d07c
AndréMeyer <andre.meyer@smartconsulting.com.br>
2023-06-24 17:56:12 -0300

    views

--- src/Views/Mail.tsx
+++ src/Views/Mail.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { API } from "../services/axios";
 import { Page } from "../components/Page";
 import { LoadButton } from "../components/Buttons/LoadButton";
@@ -7,7 +7,9 @@ import { TBody, THead, Table, Td, Th, Tr } from "../components/Table";
 import { Form } from "@unform/web";
 import { FormHandles } from "@unform/core";
 import { SelectInput } from "../components/Forms/Select";
-import { MenuItem } from "@mui/material";
+import { Grid, IconButton, MenuItem } from "@mui/material";
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { Modal } from "../components/Modal";
 
 export const Mail = () => {
   const formRef = useRef<FormHandles>(null);
@@ -39,21 +41,51 @@ export const Mail = () => {
     console.log(data);
   }
 
+  const [ModalTeste, SetModalTeste] = useState<boolean>(false);
+  
+  const handleOpenModal = () => {
+    SetModalTeste(true);
+  };
+  const handleCloseModal = () => {
+    SetModalTeste(false);
+  };
+
   return (
     <Page Title="E-mails">
-      <div style={{ margin: '20px 0' }}>
-        <Form ref={formRef} onSubmit={handleSubmit} style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-          <Input name="titulo" label="Título" variant="outlined" style={{ width: '500px' }}/>
-          <Input name="email" label="E-mail" variant="outlined" style={{ width: '500px' }}/>
-          <Input name="status" label="Status" variant="outlined" style={{ width: '300px' }}/>
-          <LoadButton variant="contained" name="submit" title="Pesquisar" type="submit" style={{ height: '50px', width: '200px' }}/>
-        </Form>
-      </div>
+      <Form ref={formRef} onSubmit={handleSubmit}>
+        <Grid container spacing={1}
+            sx={{
+              '& > :not(style)': { marginTop: '16px' },
+            }}>
+          <Grid item xs={4}>
+            <Input name="name" label="Título" variant="outlined" fullWidth/>
+          </Grid>
+          <Grid item xs={4}>
+            <Input name="email" label="E-mail" variant="outlined" fullWidth/>
+          </Grid>
+          <Grid item xs={2}>
+            <SelectInput name="status" label="Status" variant="outlined" fullWidth>
+              <MenuItem value="true">Enviado</MenuItem>
+              <MenuItem value="false">Não Enviado</MenuItem>
+            </SelectInput>
+          </Grid>
+          <Grid item xs={1}>
+            <LoadButton variant="contained" name="submit" title="Pesquisar" type="submit" fullWidth style={{height:"54px"}}/>
+          </Grid>
+          <Grid item xs={1}>
+            <LoadButton variant="outlined" name="newMail" title="Novo" type="button" onClick={handleOpenModal} fullWidth style={{height:"54px"}}/>
+          </Grid>
+        </Grid>
+      </Form>
+
+      <Modal Title="Cadastro de Clietes" Close={handleCloseModal} open={ModalTeste} maxWidth="md" fullWidth >
+        <p>Fazer</p>
+      </Modal>
 
       <Table style={{ marginTop: '40px'}}>
         <THead>
           <tr style={{textAlign: "left"}}>
-            <Th>Título</Th>
+            <Th>Nome</Th>
             <Th>E-mail</Th>
             <Th>Status</Th>
             <Th>Ações</Th>
@@ -64,7 +96,7 @@ export const Mail = () => {
             <Td>Teste1</Td>
             <Td>Teste1</Td>
             <Td>Teste1</Td>
-            <Td><LoadButton variant="contained" name="submit" title="{}" type="submit"/></Td>
+            <Td><IconButton aria-label="delete" size="small"> <MoreVertIcon fontSize="inherit" style={{color:"green"}}/></IconButton></Td>
           </Tr>
         </TBody>
       </Table>
