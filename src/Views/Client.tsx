1eeb19dc5a6bfcd0ad6df76ae903f46826d3d5fd
AndréMeyer <andre.meyer@smartconsulting.com.br>
2023-06-29 15:59:45 -0300

    Client

--- src/Views/Client.tsx
+++ src/Views/Client.tsx
@@ -6,17 +6,38 @@ import { Input } from "../components/Forms/Input";
 import { TBody, THead, Table, Td, Th, Tr } from "../components/Table";
 import { Form } from "@unform/web";
 import { FormHandles } from "@unform/core";
-import { useSnackbar, enqueueSnackbar } from 'notistack';
+import { useSnackbar } from 'notistack';
 import { SelectInput } from "../components/Forms/Select";
 import { Grid, IconButton, MenuItem } from "@mui/material";
 import { Modal } from "../components/Modal";
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 
-
 export const Client = () => {
   const formRef = useRef<FormHandles>(null);
   const { enqueueSnackbar } = useSnackbar();
-  const [clients, setClients] = useState([]);
+
+  interface Client {
+    id: number;
+    name: string;
+    email: string;
+    phone: string;
+    document: string;
+    status: string;
+  }
+
+  const [clients, setClients] = useState<Client[]>([]);
+  const [registrationFormData, setRegistrationFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    document: '',
+  });
+  const [searchFormData, setSearchFormData] = useState({
+    name: '',
+    email: '',
+    status: '',
+    document: '',
+  });
 
   useEffect(() => {
     const fetchClients = async () => {
@@ -27,117 +48,214 @@ export const Client = () => {
         console.log(error);
       }
     };
-  
+
     fetchClients();
   }, []);
-  
 
-  /*function teste() {
-    API.get('https://jsonplaceholder.typicode.com/todos/1')
-      .then(({data}) => {
-        enqueueSnackbar({
-           message: 'Requisição API feita com sucesso',
-           variant: "success" 
-        });
-        console.log(data);
-      })
-      .catch(err => {
-        enqueueSnackbar({
-            message: 'Erro na requisição API',
-            variant: "success" 
-         });
-        console.log(err);
-      })
-  };*/
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
 
-  useEffect(() => {
-    const exhttp = () => {
-      API.get('https://localhost:7182/api/ClientCrm')
-        .then(({data}) => {
-          enqueueSnackbar({
-            message: 'Requisição API feita com sucesso',
-            variant: "success" 
-          });
-          console.log(data);
-        })
-        .catch(err => {
-          enqueueSnackbar({
-            message: 'Erro na requisição API',
-            variant: "success" 
-          });
-          console.log(err);
-        })
-    }
-    return () => exhttp();
-  }, []);
-
-  function handleSubmit(data: any) {
-    console.log(data);
-  }
-
-  const [ModalTeste, SetModalTeste] = useState<boolean>(false);
-  
   const handleOpenModal = () => {
-    SetModalTeste(true);
+    setModalOpen(true);
   };
+
   const handleCloseModal = () => {
-    SetModalTeste(false);
+    setModalOpen(false);
   };
+
+  const formSearchRef = useRef<FormHandles>(null);
+  const formModalRef = useRef<FormHandles>(null);
+
+  function handleSearch(data: any) {
+    const { name, email, status } = data;
+  
+    const filteredClients = clients.filter((client) => {
+      const clientName = client.name.toLowerCase();
+      const clientEmail = client.email.toLowerCase();
+      const clientStatus = client.status === "true" ? "Ativo" : "Inativo";
+  
+      const nameMatch = clientName.includes(name.toLowerCase());
+      const emailMatch = clientEmail.includes(email.toLowerCase());
+      const statusMatch = status === "" || clientStatus.includes(status);
+  
+      return nameMatch && emailMatch && statusMatch;
+    });
+  
+    setClients(filteredClients);
+  }  
+  
+  function handleSubmit(data: any) {
+    const newClient = {
+      id: clients.length + 1,
+      name: data.name,
+      email: data.email,
+      phone: data.phone,
+      document: data.document,
+      status: "Ativo",
+    };
+  
+    setClients([...clients, newClient]);
+  
+    enqueueSnackbar("Cliente cadastrado com sucesso!", { variant: "success" });
+  
+    setRegistrationFormData({
+      name: '',
+      email: '',
+      phone: '',
+      document: '',
+    });
+  }
+  
+
 
   return (
-
     <Page Title="Clientes">
-      <Form ref={formRef} onSubmit={handleSubmit}>
-        <Grid container spacing={1}
-            sx={{
-              '& > :not(style)': { marginTop: '16px' },
-            }}>
+      <Form ref={formSearchRef} onSubmit={handleSearch}>
+        <Grid
+          container
+          spacing={1}
+          sx={{
+            '& > :not(style)': { marginTop: '16px' },
+          }}
+        >
           <Grid item xs={4}>
-            <Input name="name" label="Nome" variant="outlined" fullWidth/>
+            <Input
+              name="name"
+              label="Nome"
+              variant="outlined"
+              fullWidth
+              value={searchFormData.name}
+              onChange={(e) =>
+                setSearchFormData({ ...searchFormData, name: e.target.value })
+              }
+            />
           </Grid>
           <Grid item xs={4}>
-            <Input name="email" label="E-mail" variant="outlined" fullWidth/>
+            <Input
+              name="email"
+              label="E-mail"
+              variant="outlined"
+              fullWidth
+              value={searchFormData.email}
+              onChange={(e) =>
+                setSearchFormData({ ...searchFormData, email: e.target.value })
+              }
+            />
           </Grid>
           <Grid item xs={2}>
-            <SelectInput name="status" label="Status" variant="outlined" fullWidth>
+            <SelectInput
+              name="status"
+              label="Status"
+              variant="outlined"
+              fullWidth
+              value={searchFormData.status}
+              onChange={(e) =>
+                setSearchFormData({ ...searchFormData, status: e.target.value })
+              }
+            >
+              <MenuItem value="">Todos</MenuItem>
               <MenuItem value="true">Ativo</MenuItem>
               <MenuItem value="false">Inativo</MenuItem>
             </SelectInput>
           </Grid>
           <Grid item xs={1}>
-            <LoadButton variant="contained" name="submit" title="Pesquisar" type="submit" fullWidth style={{height:"54px"}}/>
+            <LoadButton
+              variant="contained"
+              name="submit"
+              title="Pesquisar"
+              type="submit"
+              fullWidth
+              style={{ height: "54px" }}
+            />
           </Grid>
           <Grid item xs={1}>
-            <LoadButton variant="outlined" name="cadastrar" title="Cadastrar" type="button" onClick={handleOpenModal} fullWidth style={{height:"54px"}}/>
+            <LoadButton
+              variant="outlined"
+              name="cadastrar"
+              title="Cadastrar"
+              type="button"
+              onClick={handleOpenModal}
+              fullWidth
+              style={{ height: "54px" }}
+            />
           </Grid>
         </Grid>
       </Form>
 
-      <Modal Title="Cadastro de Clietes" Close={handleCloseModal} open={ModalTeste} maxWidth="md" fullWidth>
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <Grid container spacing={2} justifyContent="center" alignItems="center">
+      <Modal
+        Title="Cadastro de Clientes"
+        Close={handleCloseModal}
+        open={modalOpen}
+        maxWidth="md"
+        fullWidth
+      >
+        <Form ref={formModalRef} onSubmit={handleSubmit}>
+          <Grid
+            container
+            spacing={2}
+            justifyContent="center"
+            alignItems="center"
+          >
             <Grid item xs={11}>
-              <Input name="name" label="Nome" variant="outlined" fullWidth/>
+              <Input
+                name="name"
+                label="Nome"
+                variant="outlined"
+                fullWidth
+                value={registrationFormData.name}
+                onChange={(e) =>
+                  setRegistrationFormData({ ...registrationFormData, name: e.target.value })
+                }
+              />
             </Grid>
             <Grid item xs={11}>
-              <Input name="email" label="E-mail" variant="outlined" fullWidth/>
+              <Input
+                name="email"
+                label="E-mail"
+                variant="outlined"
+                fullWidth
+                value={registrationFormData.email}
+                onChange={(e) =>
+                  setRegistrationFormData({ ...registrationFormData, email: e.target.value })
+                }
+              />
             </Grid>
             <Grid item xs={11}>
-              <Input name="phone" label="Telefone" variant="outlined" fullWidth/>
+              <Input
+                name="phone"
+                label="Telefone"
+                variant="outlined"
+                fullWidth
+                value={registrationFormData.phone}
+                onChange={(e) =>
+                  setRegistrationFormData({ ...registrationFormData, phone: e.target.value })
+                }
+              />
             </Grid>
             <Grid item xs={11}>
-              <Input name="document" label="CPF" variant="outlined" fullWidth/>
+              <Input
+                name="document"
+                label="CPF"
+                variant="outlined"
+                fullWidth
+              />
             </Grid>
             <Grid item xs={11} style={{ marginBottom: '30px' }}>
-              <LoadButton variant="contained" name="submit" title="Cadastrar" type="submit" fullWidth style={{height:"54px"}}/>
+              <LoadButton
+                variant="contained"
+                name="submit"
+                title="Cadastrar"
+                type="submit"
+                fullWidth
+                style={{ height: "54px" }}
+              />
             </Grid>
           </Grid>
         </Form>
       </Modal>
 
-      <Table style={{ marginTop: '40px'}}>
+      <Table style={{ marginTop: '40px' }}>
         <THead>
-          <tr style={{textAlign: "left"}}>
+          <tr style={{ textAlign: "left" }}>
             <Th>Nome</Th>
             <Th>E-mail</Th>
             <Th>Telefone</Th>
@@ -153,17 +271,16 @@ export const Client = () => {
               <Td>{client.phone}</Td>
               <Td>{client.status}</Td>
               <Td>
-              <IconButton aria-label="delete" size="small">
-                <MoreVertIcon fontSize="inherit" style={{ color: "green" }} />
-              </IconButton>
+                <IconButton aria-label="delete" size="small">
+                  <MoreVertIcon fontSize="inherit" style={{ color: "green" }} />
+                </IconButton>
               </Td>
             </Tr>
           ))}
         </TBody>
-
       </Table>
     </Page>
-  )
-}
+  );
+};
 
 export default Client;
